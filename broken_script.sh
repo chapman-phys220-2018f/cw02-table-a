a4cd261954ef6dbdf36ea10ab49acdf49944d2a5
Jack Savage <jsavage@chapman.edu>
2018-09-06 17:24:22 +0000

    Fixed broken script!

--- broken_script.sh
+++ broken_script.sh
@@ -1,12 +1,12 @@
 #/bin/bsh
 
 # You will change this line later
-eco "This script hs typs. Pleez fix them."
+echo "This script has typos. Please fix them."
 # You will also change this line later
 
 echo "Positive integers from 005 to 100:"
-for i in ${seq -w 5 100); do
-    echo i
-fi
+for i in $(seq -s " " 5 100); do
+    echo $i
+done
 
 echo "This script is now fixed."
